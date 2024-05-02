99395729ce532473ae04b21a786481e764e9c491
iansaggior <ian.saggior.pereira@gmail.com>
2024-05-02 00:17:17 -0300

    ProdutoRepositorio : implementado busca por coringa (BuscarProdutoPorCoringa)

--- SistemaProdutos/Repositorios/Interfaces/IProdutoRepositorio.cs
+++ SistemaProdutos/Repositorios/Interfaces/IProdutoRepositorio.cs
@@ -6,13 +6,14 @@ namespace SistemaProdutos.Repositorios.Interfaces
     {
         Task<List<ProdutoModel>> BuscarTodosProdutos();
         Task<ProdutoModel> BuscarProdutoPorId(int id);
+        Task<List<ProdutoModel>> BuscarProdutoPorCoringa(string coriga);
         Task<ProdutoModel> AdicionarProduto(ProdutoModel produto);
         Task<ProdutoModel> AtualizarProdutoPorId(ProdutoModel produto, int id);
         Task<bool> InativarProduto(int id);
 
         Task<bool> AddQuantidadeProduto(int ProdutoId, decimal qtde);
         Task<bool> RemovQuantidadeProduto(int ProdutoId, decimal qtde);
-        Task<List<ProdutoModel>> TesteQuerySemParametro();
-        Task<ProdutoModel> TesteQueryComParametro(int id);
+        //Task<List<ProdutoModel>> TesteQuerySemParametro();
+        //Task<ProdutoModel> TesteQueryComParametro(int id);
     }
 }
