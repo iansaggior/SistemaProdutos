7db64bfb0484b84ab344bbf033f8ddd4a2344eb5
iansaggior <ian.saggior.pereira@gmail.com>
2024-05-05 15:50:39 -0300

    implementado o método UltimasMovimentacoes.  Agora é possivel visualizar as ultimas movimentações feitas, ordenando pelo MovID decrescente

--- SistemaProdutos/Repositorios/Interfaces/ILogMovimentosRepositorio.cs
+++ SistemaProdutos/Repositorios/Interfaces/ILogMovimentosRepositorio.cs
@@ -6,6 +6,8 @@ namespace SistemaProdutos.Repositorios.Interfaces
     {
         Task<List<LogMovimentoModel>> UltimasMovimentacoes(DateTime dtInicio, DateTime dtFinal);
         Task<List<LogMovimentoModel>> UltimasMovimentacoes(DateTime dtInicio, DateTime dtFinal, int id);
+        Task<List<LogMovimentoModel>> UltimasMovimentacoes(string type);
+        Task<List<LogMovimentoModel>> UltimasMovimentacoes();
 
     }
 }
